62deecafd71186cde281368227f63d2cf4063e2a
Hassaan Farid <hassaanali994@gmail.com>
2025-12-01 10:23:24 +0500

    class_1to4

---
+++ class1_Chapter_1to4/app.js
@@ -0,0 +1,63 @@
+// // chapter 1 - Alert Function
+
+
+// alert("Hello World!")
+// alert("Hello Saylani Teacher! Welcome to JavaScript Classes.");
+
+
+
+
+// // Chapter 2 - Variable for String
+
+
+// var sabjact = "JavaScript";
+// var username = "HASSAAN";
+// var message = "Welcome to the JavaScript World";
+// var fullMessage = username + "<br>" + message;
+// document.write(fullMessage);
+// console.log(sabjact);
+// document.write(username);
+
+
+
+
+// chapter 3 - Variable for Numbers
+
+
+
+// var num1 = 23;
+// var num = 53;
+// var sum = num + "" + "" + num1;
+
+// alert(sum)
+
+// var num1 = 23;
+// var divi = num - num1;
+// document.write(sum);
+// alert(num);
+
+
+
+
+// // chapter 4 - Variable Names: Legal & Illegal
+
+// var myName = "HASSAAN"; // legal
+// var _myName = "ALI"; // legal
+// var $myName = "IMRAN"; // legal
+// // var 1myName = "HASSAAN"; // illegal
+// // var my-Name = "HASSAAN"; // illegal
+// // var my Name = "HASSAAN"; // illegal
+
+// alert(myName);
+// alert(_myName);
+// alert($myName);
+
+
+
+
+
+
+
+
+
+
