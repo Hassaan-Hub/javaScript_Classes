b6054338b2e700d28a80a9235d2a4c7f331a24d9
Hassaan Farid <hassaanali994@gmail.com>
2025-12-10 14:42:02 +0500

    Add JavaScript exercises for chapters 14 to 16, including array manipulation and qualifications display

---
+++ assignment4_Chapter_14to16/app.js
@@ -0,0 +1,84 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+
+// var emptyArr = [];
+
+// emptyArr.push("hassaan" , "majid")
+
+// console.log(emptyArr);
+
+
+
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+
+// var emptyArr = new Array();
+// emptyArr.push("hassaan", "majid");
+
+// console.log(emptyArr);
+
+
+
+
+// 3. Declare and initialize a strings array.
+
+
+// var stringArr = ["hassaan", "majid", "ahmed", "ali"];
+
+// console.log(stringArr);
+
+
+
+
+// 4. Declare and initialize a numbers array.
+
+
+// var numberArr = [123, 321, 53]
+
+// console.log(numberArr);
+
+
+
+
+// 5. Declare and initialize a boolean array.
+
+
+// var booleanArr = [true, false, true, false];
+
+// console.log(booleanArr);
+
+
+
+
+// 6. Declare and initialize a mixed array.
+
+
+// var mixedArr = ["hassaan", true, 123]
+
+// console.log(mixedArr);
+
+
+
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"]
+
+// document.write("<h1>Qualifications:</h1>")
+
+// for (var i = 0; i < qualifications.length; i++){
+//     document.write(`${i + 1}) ${qualifications[i]} <br>`)
+// }
+
+
+
+
+
+
