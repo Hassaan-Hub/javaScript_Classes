795e1b666abd11c65140d79bbc4d1fcf77e31381
Hassaan Farid <hassaanali994@gmail.com>
2025-12-20 08:58:58 +0500

    Refactor app.js: Comment out previous exercises and add string replacement functionality

--- assignment6_Chapter_21to25/app.js
+++ assignment6_Chapter_21to25/app.js
@@ -20,9 +20,133 @@
 // 3. Write a program to find the index of letter “n” in the word
 // “Pakistani” and display the result in your browser .
 
-var country = "Pakistani"
+// var country = "Pakistani"
 
-document.write(`Strings: ${country}<br> index of 'n': ${country.indexOf("n")}`)
+// document.write(`Strings: ${country}<br> index of 'n': ${country.indexOf("n")}`)
+
+
+
+
+// 4. Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+
+// var greet = "Hello World"
+
+// var user = prompt("Enter your index number")
+
+// var ind = greet.lastIndexOf(user)
+
+// document.write(`String: ${greet}<br>Last index of'${user}': ${ind}`)
+
+
+
+
+// 5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+
+// var country = "Pakistan"
+
+// var ind = country.indexOf('i')
+
+// var char = country.charAt(3)
+
+// document.write(`String: ${country} <br>`)
+// document.write(`Character at index ${ind}: ${char}`)
+
+
+
+
+// 6. Repeat Q1 using string concat() method.
+
+
+// var firstName = prompt("Enter your first name")
+// var lastName = prompt("Enter your last name")
+
+// var fullName = firstName.concat(" ",lastName)
+
+// document.write(`Hello ${fullName} Welcome!`)
+
+
+
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+
+// var arr  = "Hyderabad"
+
+// var rep = arr.replace("Hyder", "islam")
+
+// console.log(rep);
+
+
+
+
+// 8. Write a program to replace all occurrences of “and” in the
+// string with “&” and display the result in your browser.
+// var message = “Ali and Sami are best friends. They play cricket and
+// football together.”;
+
+
+var message = "Ali and Sami are best friends. They play cricket and football together."
+
+var rep = message.replaceAll("and" , "&")
+
+console.log(rep);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
