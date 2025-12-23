b7e581615fd674d0f9a81c6a10ca33e4dedb5339
Hassaan Farid <hassaanali994@gmail.com>
2025-12-23 09:42:33 +0500

    Add app.js and index.html files with initial exercises for chapters 21 to 26

---
+++ practice_javaScript_21to30/app.js
@@ -0,0 +1,93 @@
+// chapter 21 Changing case
+
+
+// var name = "hassaan"
+
+// var chan = name.toUpperCase()
+// var chan1 = name.toLowerCase()
+
+// console.log(chan);
+// console.log(chan1);
+
+
+
+
+// chapter 22 Strings: Measuring length and extracting parts
+
+
+// var name1 = "hassaan"
+
+// var char = name1.slice(1,4)
+// console.log(char);
+
+// var char = name1.slice(0,1)
+// var firstChar = char.toUpperCase()
+// var lastChar = name1.slice(1)
+// console.log(firstChar + lastChar);
+
+// var name1 = "hassaan"
+// var charLen = name1.length;
+
+// if(charLen > 3){
+//     console.log(name1.slice(0,3))
+// }
+
+
+
+
+// chapter 23 Strings: Finding segments
+
+
+// var nam = "World War II"
+
+// var inOf = nam.indexOf("War")
+// var lasInOf = nam.lastIndexOf("War")
+
+// console.log(inOf);
+// console.log(lasInOf);
+
+
+
+
+// chapter 24 Strings: Finding a character at a location
+
+
+// var greet = "Hello!"
+
+// for(let i = 0; i < greet.length; i++){
+//     if(greet.charAt(i) === "!"){
+//         alert("Exclamation point found!")
+//         break;
+//     }
+// }
+
+
+
+
+// chapter 25 Strings: Replacing characters
+
+
+// var arr = "world war II"
+
+// var rep = arr.replace("world", "second world")
+
+// console.log(arr);
+// console.log(rep);
+
+
+
+
+// chapter 26 Rounding numbers
+
+
+
+
+
+
+
+
+
+
+
+
+
