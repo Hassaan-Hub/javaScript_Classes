5952e22f5dca0b83cff0e4bb7db73e7f0e88edda
Hassaan Farid <hassaanali994@gmail.com>
2025-12-27 16:37:09 +0500

    Add additional exercises for date manipulation and time display in app.js

--- assignment8_Chapter_31to34/app.js
+++ assignment8_Chapter_31to34/app.js
@@ -89,8 +89,87 @@
 // to represent the Date object.
 
 
-var date = new Date()
-var time = date.getTime()
+// var date = new Date()
+// var time = date.getTime()
+// var minutes = time / (1000 * 60)
+
+// document.write(`Current date: ${date}<br>`)
+// document.write(`Elapset milliseconds since january 1, 1970: ${time}<br>`)
+// document.write(`Elapset minutes since january 1, 1970: ${minutes}`)
+
+
+
+
+// 7. Write a program that tests whether it's before noon and
+// alert “Its AM” else “its PM”.
+
+
+// var date = new Date()
+// var hours = date.getHours()
+
+// if (hours < 12){
+//     alert("Its AM")
+// }else{
+//     alert("its PM")
+// }
+
+
+
+
+
+//  8. Write a program that creates a Date object for the last day
+// of the last month of 2020 and assigns it to variable named
+// laterDate.
+
+
+// var date = new Date("Dec 31, 2020")
+// console.log("Later date: " + date);
+
+
+
+
+// 9. Create a date object of the starting date of this Ramadan
+// and alert the number of days past since 1st Ramadan?
+// Note: 1st Ramadan was on June 18, 2015
+
+
+// var ramadan = new Date("feb 18, 2026")
+// var date = new Date()
+// var ramadanTime = ramadan.getTime()
+// var dateTime = date.getTime()
+// var last = ramadanTime - dateTime
+// var days = Math.floor((last) / (1000 * 60 * 60 * 24))
+// console.log(days);
+
+
+
+
+// 10. Write a program that displays in your browser the
+// seconds that elapsed between the reference date and the
+// beginning of 2015.
+
+
+// var currentDate = new Date()
+// var date = new Date("2025")
+// var currentDateTime = currentDate.getTime()
+// var dateTime = date.getTime()
+// var lastResult = currentDateTime - date
+// var finalOut = Math.floor((lastResult) / (1000 * 60))
+
+// console.log(finalOut);
+
+
+
+
+// 11. Create a Date object for the current date and time.
+// Extract the hours, reset the date object an hour ahead and
+// finally display the date object in your browser.
+
+
+// var date = new Date()
+// var hour = date.getHours()
+// var addHour = (hour + 1)
+// console.log(addHour);
 
 
 
