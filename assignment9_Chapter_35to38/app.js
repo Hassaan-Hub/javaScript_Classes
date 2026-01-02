d52844f698834beb94d60c1d51864e2d9ec1e2af
Hassaan Farid <hassaanali994@gmail.com>
2026-01-03 02:04:32 +0500

    Add app.js and index.html files for assignment 9: Implement various functions including date display, greeting, sum, calculator, square, and factorial

---
+++ assignment9_Chapter_35to38/app.js
@@ -0,0 +1,110 @@
+// 1. Write a function that displays current date & time in your
+// browser.
+
+
+// function date(){
+//     let date = new Date()
+//     console.log(date);
+// }
+// date()
+
+
+
+
+// 2. Write a function that takes first & last name and then it
+// greets the user using his full name.
+
+
+// function greet(firstName , lastName){
+//     console.log(`${firstName + " " + lastName} Welcome to over web page!`);
+// }
+
+// greet("muhammad", "haroon")
+
+
+
+
+// 3. Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
+
+
+// function sum(num1, num2){
+//     let sum = num1 + num2
+//     return sum;
+// }
+
+// var user = Number(prompt("Enter your first number"))
+// var user1 = Number(prompt("Enter your second number"))
+// var result = sum(user, user1)
+// console.log(`${user} + ${user1} = ${result}`);
+
+
+
+
+// 4. Calculator:
+// Write a function that takes three arguments num1, num2
+// & operator & compute the desired operation. Return and
+// show the desired result in your browser.
+
+
+// function calculator(num1, operater, num2){
+//     var result; 
+//     if(operater == "+"){
+//         result = num1 + num2
+//     }else if(operater == "-"){
+//         result = num1 - num2
+//     }else if(operater == "*"){
+//         result = num1 * num2
+//     }else if(operater == "%"){
+//         result = num1 % num2
+//     }else if(operater == "/"){
+//         result = num1 / num2
+//     }else if(operater == "**"){
+//         result = num1 ** num2
+//     }else{
+//         alert("plzz enter a valid number and operater")
+//     }
+//     return result;
+// }
+
+// var user1 = Number(prompt("Enter your first number"))
+// var userOpera = prompt("your operater(+, -, *, **, /, %)")
+// var user2 = Number(prompt("Enter your second number"))
+
+// let finalResult = calculator(user1, userOpera, user2)
+
+// console.log(`${user1} ${userOpera} ${user2} = ${finalResult};`);
+
+
+
+
+// 5. Write a function that squares its argument.
+
+
+// function square(num){
+//     return num * num
+// }
+
+// var user = Number(prompt("Enter your number and get your square number"))
+
+// var result = square(user)
+// console.log(`${result}`);
+
+
+
+
+// 6. Write a function that computes factorial of a number.
+
+
+// function factorial(num1){
+//     var result = 1
+
+//     for(var i = 0; i < num1; i++){
+//         result = result + i
+//     }
+//     return result
+// }
+
+// var final = factorial(4)
+
+// console.log(final);
