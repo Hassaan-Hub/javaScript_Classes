37cd299e15d3e988dd6734cf94bf23ca242e0a41
Hassaan Farid <hassaanali994@gmail.com>
2026-01-11 21:09:57 +0500

    Add initial HTML structure and image files for Assignment 11 (Chapters 43 to 48)

---
+++ assignment11_Chapter_43to48/app.js
@@ -0,0 +1,19 @@
+// 1. Show an alert box on click on a link.
+
+// inside html
+
+
+
+
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+
+
+
+
+
+
+
+
+
