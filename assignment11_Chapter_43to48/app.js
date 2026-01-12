ec46ed4eecbf990f87f47696a00b0f09ad75f719
Hassaan Farid <hassaanali994@gmail.com>
2026-01-12 13:54:57 +0500

    Add counter functionality and update HTML structure for Assignment 11

--- assignment11_Chapter_43to48/app.js
+++ assignment11_Chapter_43to48/app.js
@@ -11,6 +11,56 @@
 
 
 
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.
+
+
+// function deleteRow(button) {
+//   button.parentNode.parentNode.style.display = "none";
+// }
+
+// function deleteAll() {
+//   document.getElementById("studentTable").style.display = "none";
+// }
+
+// function showAll() {
+//   document.getElementById("studentTable").style.display = "table";
+  
+//   var rows = document.querySelectorAll("#studentTable tr");
+  
+//   for (var i = 0; i < rows.length; i++) {
+//     rows[i].style.display = "table-row";
+//   }
+// }
+
+
+
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+// inside html
+
+
+
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+
+
+var count = 0;
+
+function counterIncrease(){
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+function counterDecrease(){
+    count--;
+    document.getElementById("counter").innerHTML = count;
+}
 
 
 
