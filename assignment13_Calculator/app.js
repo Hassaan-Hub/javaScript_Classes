dc478e73d125575b7e8019d7ce18dee8c06efc2a
Hassaan Farid <hassaanali994@gmail.com>
2026-01-18 22:12:17 +0500

    updated

--- assignment13_Calculator/app.js
+++ assignment13_Calculator/app.js
@@ -1,4 +1,19 @@
-function inputNumber(this){
+function inputNumber(num){
     let result = document.getElementById('input');
-    result.value += this.value
+    result.value += num;
+}
+
+function clearAll(){
+    let result = document.getElementById("input");
+    result.value = ""
+}
+
+function calculate(){
+    let result = document.getElementById("input");
+    result.value = eval(result.value)
+}
+
+function backspase(){
+    let result = document.getElementById("input");
+    result.value = result.value.slice(0, -1);
 }
