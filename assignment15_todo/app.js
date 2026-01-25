0e823384bddf266b6455df564d567afc381de8a3
Hassaan Farid <hassaanali994@gmail.com>
2026-01-25 23:08:52 +0500

    Add initial implementation of Todo app with add, edit, and delete functionalities

---
+++ assignment15_todo/app.js
@@ -0,0 +1,43 @@
+function addTodo(){
+    var input = document.getElementById("input")
+    var list = document.getElementById("list")
+
+    var li = document.createElement("li")
+    var delBtn = document.createElement("button")
+    var editBtn = document.createElement("button")
+
+    var liText = document.createTextNode(input.value)
+    var delBtnText = document.createTextNode("Delete")
+    var editBtnText = document.createTextNode("Edit")
+
+    delBtn.setAttribute("class", "btn1")
+    editBtn.setAttribute("class", "btn1")
+    delBtn.setAttribute("onclick", "delBtn()")
+    editBtn.setAttribute("onclick", "editBtn(this)")
+
+    li.appendChild(liText)
+    delBtn.appendChild(delBtnText)
+    editBtn.appendChild(editBtnText)
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+    list.appendChild(li)
+
+    input.value = ""
+}
+
+
+function delBtn(){
+    event.target.parentNode.remove();
+}
+
+function editBtn(e){
+    var oldText = e.parentNode.firstChild.nodeValue;
+    var newText = prompt("Enter your Edit value", oldText)
+
+    if(newText != "" && newText != null){
+        e.parentNode.firstChild.nodeValue = newText;
+    }
+    
+}
+
+
