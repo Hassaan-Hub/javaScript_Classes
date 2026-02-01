d237627ea1fe2f460c93a0dda7f37a813333a614
Hassaan Farid <hassaanali994@gmail.com>
2026-02-02 03:51:33 +0500

    Add initial HTML structure for Products page with a placeholder for cards

---
+++ assignment16_Products/app.js
@@ -0,0 +1,63 @@
+var products = {
+    cars: {
+        honda: {
+            model: "Civic",
+            year: 2026,
+            color: "black",
+            img: "images/civic2026.avif"
+        },
+        kia: {
+            model: "Sportage",
+            year: 2026,
+            color: "white",
+            img: "https://kiamotors-portqasim.com/wp-content/uploads/2020/07/Kia-Sportage-Alpha-1-1-798x466.jpg"
+        },
+        toyota: {
+            model: "Grendis",
+            year: 2022,
+            color: "blue",
+            img: "https://imgcdn.zigwheels.pk/large/gallery/color/14/114/toyota_corolla-altis-grande_strong_blue.jpg"
+        }
+    },
+    bikes: {
+        royalEnfield: {
+            model: "Royal Enfield Classic 350",
+            year: 2023,
+            color: "red",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV-Mb8zjzIImAk3qr5DrWVSu42Vczsz5PxGA&s"
+        },
+        yamaha: {
+            model: "Yamaha YZF R15",
+            year: 2024,
+            color: "blue",
+            img: "https://i.pinimg.com/736x/57/15/de/5715defee6441aa788b15884a1467718.jpg"
+        },
+        suzuki: {
+            model: "Suzuki Gixxer",
+            year: 2022,
+            color: "black",
+            img: "https://media.istockphoto.com/id/594474448/photo/suzuki-gsx-r750.jpg?s=612x612&w=0&k=20&c=uCwfSf44Rya81hZyyxPTqVD815KOSpQ9uZSuZ4WUJ5Y="
+        }
+    },
+    phons:{
+        summsung:{
+            model: "Galaxy S26 Ultra",
+            year: 2026,
+            color: "black",
+            img: "https://phonebolee.com/images/Samsung-Galaxy-S26-Ultra.jpg"
+        },
+        apple:{
+            model: "iPhone 17 Pro max",
+            year: 2026,
+            color: "orange",
+            img: "https://www.mobiledokan.com/media/apple-iphone-17-pro-max-cosmic-orange-official-image.webp"
+        },
+        google:{
+            model: "Pixel 10 Pro",
+            year: 2026,
+            color: "blue",
+            img: "https://eezepc.com/wp-content/uploads/2025/09/google1.webp"
+        }
+    }
+}
+
