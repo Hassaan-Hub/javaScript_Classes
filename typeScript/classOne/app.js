c180c4669ef66e05c9ace32d72b385ea36e15007
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-06-02 14:37:53 +0500

    add initial JavaScript and TypeScript files with basic functionality

---
+++ typeScript/classOne/app.js
@@ -0,0 +1,11 @@
+"use strict";
+let name = "hassaan";
+console.log(name);
+const student = () => {
+    let name = "hassaan";
+    console.log(name);
+};
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(3, 4));
---
+++ typeScript/classOne/app.ts
@@ -0,0 +1,17 @@
+// let name: string = "hassaan"
+
+// console.log(name)
+
+
+
+// const student = () => {
+//     let name: string = "hassaan"
+//     console.log(name);
+// }
+
+
+
+// function sum(a: number, b: number): number {
+//     return a + b
+// }
+// console.log(sum(3, 4));
