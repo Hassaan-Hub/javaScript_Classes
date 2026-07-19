4d1059f8d1ba964db554f00d050e9546eaaec1ac
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-07-19 16:37:02 +0500

    updated

---
+++ react/class-02/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <div className="main">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ react/class-02/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import './Card.css'
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <h2>Card Component</h2>
+            <hr />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae, autem
+                obcaecati! 
+            </p>
+        </div>
+    )
+}
+
+export default Card
