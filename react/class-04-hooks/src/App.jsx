1aaf58669e62ecf402f5e026748b4830d08cd4dc
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-08-24 22:34:27 +0500

    feat: implement hooks for counter and input fields, remove Tailwind CSS import

--- react/class-04-hooks/src/App.jsx
+++ react/class-04-hooks/src/App.jsx
@@ -1,13 +1,49 @@
-import React, { useRef, useState } from 'react'
+import React, { useId, useRef, useState } from 'react'
 
 const App = () => {
 
-  const [counter, setCounter] = useState(second)
+  const [counter, setCounter] = useState(0)
+  const nameId = useId()
+  const passId = useId()
   const headingRef = useRef()
-  
+  const headingRef1 = useRef()
+
   return (
     <div>
-      <h1 ref={headingRef}></h1>
+      <h2 ref={headingRef1}>hello world</h2>
+      <h1 ref={headingRef}>{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1)
+          console.log(headingRef.current)
+          console.log(headingRef1.current)
+        }}
+      >
+        increase
+      </button>
+
+      <button
+        disabled={counter < 1}
+        onClick={() => setCounter(counter - 1)}
+      >
+        decrease
+      </button>
+
+
+        <br />
+        <br />
+      <label id='nameId'>
+        name
+        <br />
+        <input type="email" id="nameId" />
+      </label>
+
+        <br />
+      <label id='passId'>
+        password
+        <br />
+        <input type="password" id="passId" />
+      </label>
     </div>
   )
 }
