177f5690e000164ef8b40f820d6ed1a6e0fc3556
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-09-13 18:32:28 +0500

    update

---
+++ backend/class-01-express/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+
+app.post('/', (req, res)=>{
+    users.push(req.body)
+    res.status(201).json({
+        message: "users add successfully"
+    })
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the Express server!",
+        users: users
+    })
+});
+
+
+module.exports = app;
